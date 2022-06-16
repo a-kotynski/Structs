658634263d52f840de7e3564cbcc5e4a00694967
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-16 06:39:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public struct RgbColor
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+    // most of the times using structures is pretty pointless
+    // structs are good for defining small, lightweight object
+}
